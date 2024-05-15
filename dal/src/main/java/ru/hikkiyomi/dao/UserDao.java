65bc8a6e80ecd9526d4ff3e5dd31539a22296fd5
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-05-15 22:57:58 +0300

    fix: add needed methods to user repo

--- dal/src/main/java/ru/hikkiyomi/dao/UserDao.java
+++ dal/src/main/java/ru/hikkiyomi/dao/UserDao.java
@@ -3,5 +3,9 @@ package ru.hikkiyomi.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hikkiyomi.model.User;
 
+import java.util.Optional;
+
 public interface UserDao extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }

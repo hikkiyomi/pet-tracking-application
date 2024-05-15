17f92a1ca4cb20071e6766b61e96e2f10994fd39
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-05-15 23:03:47 +0300

    fix: add missing annotation to user repo

--- dal/src/main/java/ru/hikkiyomi/dao/UserDao.java
+++ dal/src/main/java/ru/hikkiyomi/dao/UserDao.java
@@ -1,10 +1,12 @@
 package ru.hikkiyomi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hikkiyomi.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserDao extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);

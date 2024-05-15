f4380d677dda57236ebe8e734549f9297d408c82
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-05-15 23:53:26 +0300

    refactor(user service): rename method to satisfy semantics

--- bll/src/main/java/ru/hikkiyomi/service/UserService.java
+++ bll/src/main/java/ru/hikkiyomi/service/UserService.java
@@ -3,7 +3,6 @@ package ru.hikkiyomi.service;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.hikkiyomi.dao.UserDao;
@@ -45,7 +44,7 @@ public class UserService implements CommonCrudService<User> {
         return userDao.findAll();
     }
 
-    public User findByUsername(String username) {
+    public User getByUsername(String username) {
         return userDao.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
     }

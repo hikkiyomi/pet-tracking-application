32a93e8f170c07bb3a131985f5d0cbb87443c0f4
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-05-24 17:18:47 +0300

    fix: redo modules

--- kitten-microservice/data-access/src/main/java/ru/hikkiyomi/repositories/KittenDao.java
+++ kitten-microservice/data-access/src/main/java/ru/hikkiyomi/repositories/KittenDao.java
@@ -1,7 +1,9 @@
 package ru.hikkiyomi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hikkiyomi.models.Kitten;
 
+@Repository
 public interface KittenDao extends JpaRepository<Kitten, Long> {
 }
---
+++ owner-microservice/business-logic/src/main/java/ru/hikkiyomi/OwnerApplication.java
@@ -0,0 +1,11 @@
+package ru.hikkiyomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OwnerApplication.class, args);
+    }
+}
